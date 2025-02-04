1eebdf7ce9759b3eec2e163b8127e2cf231404ce
Dayana <dayana82006@gmail.com>
2025-02-04 10:23:31 -0500

    feat: rename

--- app3.js
+++ app3.js
@@ -4,9 +4,9 @@
 document.addEventListener('DOMContentLoaded', async () => {
     // Importar los componentes dinámicamente
     await Promise.all([
-        import('./components/teamComponent.js'),
-        import('./components/adminTeamComponent.js'),
-        import('./components/driversComponent.js')
+        import('./components3/teamComponent.js'),
+        import('./components3/adminTeamComponent.js'),
+        import('./components3/driversComponent.js')
     ]);
     
     const navLinks = document.querySelectorAll('.nav-link');
