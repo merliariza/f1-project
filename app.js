0e2d347d18cd88d3b449a27d32902f6edf00ab3c
Dayana <dayana82006@gmail.com>
2025-02-04 09:57:34 -0500

    feat: update

--- app.js
+++ app.js
@@ -1,1 +1,8 @@
-import './src/components/mainComponent.js'
+//import './src/components/mainComponent.js'
+import './src/components/vehiclesMenuComponent.js'
+//import './src/components/vehiclesComponent.js'
+import './Models/vehicleModel.js'
+import './src/js/gestor.js'
+import './src/components/formDataDriving.js'
+import { setupVehicleClickListener } from './src/js/gestor.js';
+setupVehicleClickListener();
--- app2.js
+++
@@ -1,7 +0,0 @@
-import './src/components/vehiclesMenuComponent.js'
-//import './src/components/vehiclesComponent.js'
-import './Models/vehicleModel.js'
-import './src/js/gestor.js'
-import './src/components/formDataDriving.js'
-import { setupVehicleClickListener } from './src/js/gestor.js';
-setupVehicleClickListener();
